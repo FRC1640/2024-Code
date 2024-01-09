899af8b6aabdc3efb389359c33a07058ee5ed49c
jdugan0 <nitsujwriter@gmail.com>
2024-01-08 21:03:00 -0500

    advantagekit changes

---
+++ src/main/java/frc/robot/drive/ModuleIO.java
@@ -0,0 +1,10 @@
+package frc.robot.drive;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ModuleIO extends AutoCloseable{
+    @AutoLog
+    public static class MoudleIOInputs {
+        
+    }
+}
