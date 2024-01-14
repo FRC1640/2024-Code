9926e8ebbe81edc4b8cf808c1af5244c8443d4b6
zoe99999999 <zoe@ebers.com>
2024-01-14 17:59:27 -0500

    working match timer and endgame indication girlboss

--- src/main/java/frc/robot/DashboardInit.java
+++ src/main/java/frc/robot/DashboardInit.java
@@ -14,18 +14,15 @@ public class DashboardInit {
     }
 
     public static void matchInit() {
-    // testing the theory: Endgame boolean
+    // ENDGAME INDICATOR (Number is 21 because shuffleboard is pretending the = in <= doesn't exist)
         ShuffleboardTab teleop = Shuffleboard.getTab("Teleop");
         teleop.addBoolean("Endgame", () -> DriverStation.getMatchTime() <= 21 && DriverStation.isTeleop()).withSize(3, 3).withPosition(0, 1);
-        // Number is 21 because shuffleboard is pretending the = in <= doesn't exist
     // BIG FEATURE 1: Big ol' match timer
         teleop.addDouble("Match Timer", () -> Math.round(DriverStation.getMatchTime()*10000)/10000).withSize(2, 1).withPosition(0, 0);
     // BIG FEATURE 2: Is the robot holding a note?
-        // teleop.addBoolean("Note?"() -> [if sensor yes? - is there a sensor
-        // here?]).withSize(5,5).withPosition(2, 0);
+        // teleop.addBoolean("Note?"() -> [if sensor yes? - is there a sensor here?]).withSize(x,y).withPosition(x, y);
     // BIG FEATURE 3: camera feed
-        // DON'T WORRY I WILL CHANGE THE TITLE I KNOW ITS DUMB ITS A PLACEHOLDER (so are
-        // the camera name and url in case you couldn't tell)
+        // DON'T WORRY I WILL CHANGE THE TITLE I KNOW ITS DUMB ITS A PLACEHOLDER (so are the camera name and url in case you couldn't tell)
         //teleop.addCamera("Robot Vision 👀", "camera name", "camera url").withSize(5, 5).withPosition(0, 1);
 
     // Justin suggesion: Limelight info? but like what info tho
