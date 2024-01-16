dfb41837aa8b7b2118be88581de89a516bc3a61e
renieharris <renieharris124@gmail.com>
2024-01-15 21:44:12 -0500

    formatting changes to vision

--- src/main/java/frc/robot/sensors/Vision/Vision.java
+++ src/main/java/frc/robot/sensors/Vision/Vision.java
@@ -17,18 +17,18 @@ public class Vision extends PeriodicBase {
     private VisionIO io;
     private VisionIOInputsAutoLogged inputs = new VisionIOInputsAutoLogged();
     
-    private Translation2d botTran2d;
-    private Rotation2d botRotation2d;
-    private Pose2d botPose2d;
+    private Translation2d aprilTagBotTran2d;
+    private Rotation2d aprilTagBotRotation2d;
+    private Pose2d aprilTagBotPose2d;
     private double latency;
 
 
     public Vision(VisionIO io){
         this.io = io;
 
-        botTran2d = new Translation2d(inputs.botPose[0], inputs.botPose[1]);
-        botRotation2d = new Rotation2d(inputs.botPose[3], inputs.botPose[4]);
-        botPose2d = new Pose2d(botTran2d, botRotation2d);
+        aprilTagBotTran2d = new Translation2d(inputs.botPose[0], inputs.botPose[1]);
+        aprilTagBotRotation2d = new Rotation2d(inputs.botPose[3], inputs.botPose[4]);
+        aprilTagBotPose2d = new Pose2d(aprilTagBotTran2d, aprilTagBotRotation2d);
 
         latency = Timer.getFPGATimestamp() - (inputs.botPose[6]/1000.0);
 
@@ -42,7 +42,7 @@ public class Vision extends PeriodicBase {
    //pseudocode for the "latency" component of WPILib' addVisionMeasurement(): Timer.getFPGATimestamp() - (tl/1000.0) - (cl/1000.0) or Timer.getFPGATimestamp() - (botpose[6]/1000.0) 
 
    public void addVisionMeasurement (SwerveDrivePoseEstimator poseEstimator){
-    poseEstimator.addVisionMeasurement(botPose2d, latency);
+    poseEstimator.addVisionMeasurement(aprilTagBotPose2d, latency);
    }
 
    
--- src/main/java/frc/robot/sensors/Vision/VisionIOLimelight.java
+++ src/main/java/frc/robot/sensors/Vision/VisionIOLimelight.java
@@ -7,41 +7,36 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-
 public class VisionIOLimelight implements VisionIO {
 
     @Override
     public void updateInputs(VisionIOInputs inputs) {
         NetworkTable networkTable = NetworkTableInstance.getDefault().getTable("limelight");
-        double[] temp = {0, 0, 0 ,0,0,0};
-        inputs.botPose = networkTable.getEntry("botpose").getDoubleArray(temp);
+        double[] emptyArray = { 0, 0, 0, 0, 0, 0 };
+        inputs.botPose = networkTable.getEntry("botpose").getDoubleArray(emptyArray);
 
-        //inputs.pipeline = networkTable.getEntry("pipeline").getInteger(0);
-        //inputs.camMode = networkTable.getEntry("camMode");
-        //inputs.ledMode = networkTable.getEntry("ledMode");
-        //inputs.horizontal = networkTable.getEntry("tx").getDouble(0);
-        //inputs.vertical = networkTable.getEntry("ta").getDouble(0);
-        //inputs.targetArea = networkTable.getEntry("ty").getDouble(0);
-        //inputs.foundTag = networkTable.getEntry("tv").getDouble(0);
-        //inputs.foundTagBool = (inputs.foundTag != 0) ? true : false;
+        // inputs.pipeline = networkTable.getEntry("pipeline").getInteger(0);
+        // inputs.camMode = networkTable.getEntry("camMode");
+        // inputs.ledMode = networkTable.getEntry("ledMode");
+        // inputs.horizontal = networkTable.getEntry("tx").getDouble(0);
+        // inputs.vertical = networkTable.getEntry("ta").getDouble(0);
+        // inputs.targetArea = networkTable.getEntry("ty").getDouble(0);
+        // inputs.foundTag = networkTable.getEntry("tv").getDouble(0);
+        // inputs.foundTagBool = (inputs.foundTag != 0) ? true : false;
 
-        //inputs.horizontalResolution = networkTable.getEntry("pipeline").getInteger(0);
-        //inputs.verticalResolution = networkTable.getEntry("pipeline").getInteger(0);
-        
-        
-        //inputs.tl = networkTable.getEntry("tl").getDouble(0);
-        //inputs.cl = networkTable.getEntry("cl").getDouble(0);
+        // inputs.horizontalResolution =
+        // networkTable.getEntry("pipeline").getInteger(0);
+        // inputs.verticalResolution = networkTable.getEntry("pipeline").getInteger(0);
 
-
-        
+        // inputs.tl = networkTable.getEntry("tl").getDouble(0);
+        // inputs.cl = networkTable.getEntry("cl").getDouble(0);
 
     }
 
-    //@Override
-    //public void setPipeline(VisionIOInputs inputs){}
-    
-    //@Override
-    //public void setCameraMode(VisionIOInputs inputs){}
+    // @Override
+    // public void setPipeline(VisionIOInputs inputs){}
 
+    // @Override
+    // public void setCameraMode(VisionIOInputs inputs){}
 
 }
