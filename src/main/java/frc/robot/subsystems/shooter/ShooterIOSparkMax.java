f3b60215f4a05e0b320da024fa5d1fccb20affb4
jdugan0 <nitsujwriter@gmail.com>
2024-01-16 17:15:07 -0500

    ShooterIO

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems.shooter;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.RobotController;
+
+public class ShooterIOSparkMax implements ShooterIO {
+    private final CANSparkMax topShooter, bottomShooter;
+
+    public ShooterIOSparkMax() {
+        topShooter = new CANSparkMax(-2, MotorType.kBrushless); // TODO ids
+        bottomShooter = new CANSparkMax(-1, MotorType.kBrushless);
+    }
+
+    @Override
+    public void setSpeedPercent(double top, double bottom) {
+        topShooter.set(top);
+        bottomShooter.set(bottom);
+    }
+
+    @Override
+    public void setVoltage(double top, double bottom) {
+        topShooter.setVoltage(top);
+        bottomShooter.setVoltage(bottom);
+    }
+
+    @Override
+    public void updateInputs(ShooterIOInputs inputs) {
+        inputs.topSpeedPercent = topShooter.get();
+        inputs.topAppliedVoltage = topShooter.getAppliedOutput() * RobotController.getBatteryVoltage();
+        inputs.topCurrentAmps = topShooter.getOutputCurrent();
+        inputs.topTempCelsius = topShooter.getMotorTemperature();
+
+        inputs.bottomSpeedPercent = bottomShooter.get();
+        inputs.bottomAppliedVoltage = bottomShooter.getAppliedOutput() * RobotController.getBatteryVoltage();;
+        inputs.bottomCurrentAmps = bottomShooter.getOutputCurrent();
+        inputs.bottomTempCelsius = bottomShooter.getMotorTemperature();
+        
+    }
+}
