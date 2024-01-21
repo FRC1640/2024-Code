3be0bea9942d77f4ef92dc76503ab6d2f09f1c5e
jdugan0 <nitsujwriter@gmail.com>
2024-01-20 22:26:04 -0500

    fixed weird formatting issues + max speed issues with rotating and moving

--- src/main/java/frc/lib/drive/DriveSubsystem.java
+++ src/main/java/frc/lib/drive/DriveSubsystem.java
@@ -59,7 +59,7 @@ public class DriveSubsystem extends SubsystemBase {
     
 
     // SwerveDriveOdometry odometry;
-    SwerveDrivePoseEstimator swervePoseEstimator; // swerve pose estimator is an alt. for swerve odometry
+    SwerveDrivePoseEstimator swervePoseEstimator; 
     SysIdRoutine sysIdRoutine;
     Pose2d odometryPose = new Pose2d();
 
@@ -105,22 +105,14 @@ public class DriveSubsystem extends SubsystemBase {
                 getModulePositionsArray(),
                 new Pose2d(),
                 VecBuilder.fill(0.05, 0.05, 0.05),
-                VecBuilder.fill(VisionConstants.xyStdDev, VisionConstants.xyStdDev, VisionConstants.thetaStdDev));// THIS
-                                                                                                                  // IS
-                                                                                                                  // SUPPOSED
-                                                                                                                  // TO
-                                                                                                                  // BE
-                                                                                                                  // THE
-                                                                                                                  // starting
-                                                                                                                  // standard
-                                                                                                                  // deviations
+                VecBuilder.fill(VisionConstants.xyStdDev, VisionConstants.xyStdDev, VisionConstants.thetaStdDev));
 
         // Configure pathplanner
         AutoBuilder.configureHolonomic(
                 this::getPose,
                 this::resetOdometry,
                 () -> SwerveDriveDimensions.kinematics.toChassisSpeeds(getActualSwerveStates()),
-                this::driveChassisSpeedsNoScaling,
+                this::driveChassisSpeedsNoScaling, //TODO is this right? maybe desaturate?
                 new HolonomicPathFollowerConfig(
                         SwerveDriveDimensions.maxSpeed,
                         SwerveAlgorithms.computeMaxNorm(SwerveDriveDimensions.positions, new Translation2d(0, 0)),
@@ -214,10 +206,10 @@ public class DriveSubsystem extends SubsystemBase {
         SwerveModuleState[] swerveModuleStates = SwerveAlgorithms.desaturated(xSpeed, ySpeed, rot,
                 gyro.getAngleRotation2d().getRadians(), fieldRelative);
 
-        frontLeft.setDesiredState(swerveModuleStates[0]);
-        frontRight.setDesiredState(swerveModuleStates[1]);
-        backLeft.setDesiredState(swerveModuleStates[2]);
-        backRight.setDesiredState(swerveModuleStates[3]);
+        frontLeft.setDesiredStatePercent(swerveModuleStates[0]);
+        frontRight.setDesiredStatePercent(swerveModuleStates[1]);
+        backLeft.setDesiredStatePercent(swerveModuleStates[2]);
+        backRight.setDesiredStatePercent(swerveModuleStates[3]);
         desiredSwerveStates = swerveModuleStates;
     }
 
@@ -231,10 +223,10 @@ public class DriveSubsystem extends SubsystemBase {
         SwerveModuleState[] swerveModuleStates = SwerveAlgorithms.desaturated(xSpeed, ySpeed, rot,
                 gyro.getAngleRotation2d().getRadians(), fieldRelative, centerOfRot);
 
-        frontLeft.setDesiredState(swerveModuleStates[0]);
-        frontRight.setDesiredState(swerveModuleStates[1]);
-        backLeft.setDesiredState(swerveModuleStates[2]);
-        backRight.setDesiredState(swerveModuleStates[3]);
+        frontLeft.setDesiredStateMetersPerSecond(swerveModuleStates[0]);
+        frontRight.setDesiredStateMetersPerSecond(swerveModuleStates[1]);
+        backLeft.setDesiredStateMetersPerSecond(swerveModuleStates[2]);
+        backRight.setDesiredStateMetersPerSecond(swerveModuleStates[3]);
         desiredSwerveStates = swerveModuleStates;
     }
 
@@ -249,41 +241,37 @@ public class DriveSubsystem extends SubsystemBase {
      *                      field.
      */
     private void drivePercentDoubleCone(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-        xSpeed = xSpeed * maxSpeed;
-        ySpeed = ySpeed * maxSpeed;
-        rot = rot * maxSpeed
+        rot = rot
                 / SwerveAlgorithms.computeMaxNorm(SwerveDriveDimensions.positions, new Translation2d(0, 0));
         SwerveModuleState[] swerveModuleStates = SwerveAlgorithms.doubleCone(xSpeed, ySpeed, rot,
                 gyro.getAngleRotation2d().getRadians(), fieldRelative);
-        frontLeft.setDesiredState(swerveModuleStates[0]);
-        frontRight.setDesiredState(swerveModuleStates[1]);
-        backLeft.setDesiredState(swerveModuleStates[2]);
-        backRight.setDesiredState(swerveModuleStates[3]);
+        frontLeft.setDesiredStatePercent(swerveModuleStates[0]);
+        frontRight.setDesiredStatePercent(swerveModuleStates[1]);
+        backLeft.setDesiredStatePercent(swerveModuleStates[2]);
+        backRight.setDesiredStatePercent(swerveModuleStates[3]);
         desiredSwerveStates = swerveModuleStates;
     }
 
     private void drivePercentDoubleCone(double xSpeed, double ySpeed, double rot, boolean fieldRelative,
             Translation2d centerOfRotation) {
-        xSpeed = xSpeed * maxSpeed;
-        ySpeed = ySpeed * maxSpeed;
-        rot = rot * maxSpeed
-                / SwerveAlgorithms.computeMaxNorm(SwerveDriveDimensions.positions, new Translation2d(0, 0));
+        rot = rot
+                / SwerveAlgorithms.computeMaxNorm(SwerveDriveDimensions.positions, centerOfRotation);
         SwerveModuleState[] swerveModuleStates = SwerveAlgorithms.doubleCone(xSpeed, ySpeed, rot,
                 gyro.getAngleRotation2d().getRadians(), fieldRelative, centerOfRotation);
-        frontLeft.setDesiredState(swerveModuleStates[0]);
-        frontRight.setDesiredState(swerveModuleStates[1]);
-        backLeft.setDesiredState(swerveModuleStates[2]);
-        backRight.setDesiredState(swerveModuleStates[3]);
+        frontLeft.setDesiredStatePercent(swerveModuleStates[0]);
+        frontRight.setDesiredStatePercent(swerveModuleStates[1]);
+        backLeft.setDesiredStatePercent(swerveModuleStates[2]);
+        backRight.setDesiredStatePercent(swerveModuleStates[3]);
         desiredSwerveStates = swerveModuleStates;
     }
 
     private void driveChassisSpeedsNoScaling(ChassisSpeeds speeds) {
         SwerveModuleState[] swerveModuleStates = SwerveAlgorithms.rawSpeeds(speeds.vxMetersPerSecond,
                 speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond);
-        frontLeft.setDesiredState(swerveModuleStates[0]);
-        frontRight.setDesiredState(swerveModuleStates[1]);
-        backLeft.setDesiredState(swerveModuleStates[2]);
-        backRight.setDesiredState(swerveModuleStates[3]);
+        frontLeft.setDesiredStateMetersPerSecond(swerveModuleStates[0]);
+        frontRight.setDesiredStateMetersPerSecond(swerveModuleStates[1]);
+        backLeft.setDesiredStateMetersPerSecond(swerveModuleStates[2]);
+        backRight.setDesiredStateMetersPerSecond(swerveModuleStates[3]);
         desiredSwerveStates = swerveModuleStates;
     }
 
