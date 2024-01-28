fc7e44840f1f953fec85ad89c3573f7b521e606e
jdugan0 <nitsujwriter@gmail.com>
2024-01-28 14:24:24 -0500

    clamp speeds method

--- src/main/java/frc/robot/subsystems/targeting/TargetingIO.java
+++ src/main/java/frc/robot/subsystems/targeting/TargetingIO.java
@@ -2,7 +2,8 @@ package frc.robot.subsystems.targeting;
 
 import org.littletonrobotics.junction.AutoLog;
 
-import edu.wpi.first.math.MathUtil; 
+import edu.wpi.first.math.MathUtil;
+import frc.robot.Constants.TargetingConstants;
 
 public interface TargetingIO {
     @AutoLog
@@ -27,13 +28,24 @@ public interface TargetingIO {
 
     public default void setTargetingVoltage(double voltage) {
         voltage = MathUtil.clamp(voltage, -12, 12);
-        setTargetingSpeedPercent(voltage/12);
+        setTargetingSpeedPercent(voltage / 12);
     }
 
     public default void setTargetingSpeedPercent(double speed) {
     }
 
     public default double getPositionAverage(double motorOneEncoderValue, double motorTwoEncoderValue) {
-        return (motorOneEncoderValue + motorTwoEncoderValue)/2;
+        return (motorOneEncoderValue + motorTwoEncoderValue) / 2;
+    }
+
+    public default double clampSpeeds(double pos, double speed) {
+        double speedClamped = speed;
+        if (pos < TargetingConstants.targetingLowerLimit) {
+            speedClamped = Math.max(speed, 0);
+        }
+        if (pos > TargetingConstants.targetingUpperLimit) {
+            speedClamped = Math.min(speed, 0);
+        }
+        return speedClamped;
     }
 }
