3653b4671039575b792d29c443e2cd26b63729bc
jdugan0 <nitsujwriter@gmail.com>
2024-01-28 14:19:35 -0500

    removed unneeded setdesiredstates auto method

--- src/main/java/frc/lib/drive/DriveSubsystem.java
+++ src/main/java/frc/lib/drive/DriveSubsystem.java
@@ -274,10 +274,10 @@ public class DriveSubsystem extends SubsystemBase {
     private void driveChassisSpeedsDesaturated(ChassisSpeeds speeds) {
         SwerveModuleState[] swerveModuleStates = SwerveAlgorithms.desaturated(speeds.vxMetersPerSecond,
                 speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond, 0, false);
-        frontLeft.setDesiredStateMetersPerSecondAuto(swerveModuleStates[0]);
-        frontRight.setDesiredStateMetersPerSecondAuto(swerveModuleStates[1]);
-        backLeft.setDesiredStateMetersPerSecondAuto(swerveModuleStates[2]);
-        backRight.setDesiredStateMetersPerSecondAuto(swerveModuleStates[3]);
+        frontLeft.setDesiredStateMetersPerSecond(swerveModuleStates[0]);
+        frontRight.setDesiredStateMetersPerSecond(swerveModuleStates[1]);
+        backLeft.setDesiredStateMetersPerSecond(swerveModuleStates[2]);
+        backRight.setDesiredStateMetersPerSecond(swerveModuleStates[3]);
         desiredSwerveStates = swerveModuleStates;
     }
 
--- src/main/java/frc/lib/drive/Module/Module.java
+++ src/main/java/frc/lib/drive/Module/Module.java
@@ -97,32 +97,6 @@ public class Module {
         io.setSteerPercentage(turnOutput);
     }
 
-    public void setDesiredStateMetersPerSecondAuto(SwerveModuleState state) { // TODO: clean up this method?
-        double dAngle = state.angle.getDegrees() - inputs.steerAngleDegrees;
-        double dAngleAbs = Math.abs(dAngle) % 360;
-        boolean flipDriveTeleop = (90.0 <= dAngleAbs) && (dAngleAbs <= 270.0);
-        double sin = Math.sin(Math.toRadians(dAngle));
-        sin = (flipDriveTeleop) ? -sin : sin;
-        double turnOutput = turningPIDController.calculate(sin, 0);
-
-        double targetSpeed = flipDriveTeleop ? state.speedMetersPerSecond : -state.speedMetersPerSecond;
-
-        //targetSpeed = targetSpeed / SwerveDriveDimensions.maxSpeed;
-
-        double pidSpeed = (driveFeedforward.calculate(targetSpeed) + 
-            drivePIDController.calculate(inputs.driveVelocityMetersPerSecond, targetSpeed)); 
-
-
-        pidSpeed = MathUtil.clamp(pidSpeed, -12, 12);
-
-        if (Math.abs(pidSpeed) < 0.05) {
-            turnOutput = 0;
-        }
-
-        io.setDriveVoltage(pidSpeed);
-        io.setSteerPercentage(turnOutput);
-    }
-
     public void setDriveVoltage(double volts) {
         io.setDriveVoltage(volts);
     }
