541613e3df6bb2f263ac7b08a1b3a95ce6bc9cc6
jdugan0 <nitsujwriter@gmail.com>
2024-02-17 15:02:20 -0500

    climber pid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -121,6 +121,7 @@ public final class Constants {
         public static PIDController targetingPID = new PIDController(0.1, 0, 0);
         public static PIDController horizontalMLVision = new PIDController(0.008, 0, 0);
         public static PIDController rotMLVision = new PIDController(0.004, 0, 0);
+        public static PIDController climberPID = new PIDController(0.01, 0, 0);
     }
     public static class FieldConstants{
         public static double height = 8.21;
