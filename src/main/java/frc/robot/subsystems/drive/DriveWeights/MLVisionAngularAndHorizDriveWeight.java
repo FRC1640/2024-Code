baf77d9c28736ce5304746a0481909e356bc2897
renieharris <renieharris124@gmail.com>
2024-02-18 16:15:17 -0500

    kind of working 2 note distinction No continue after sight loss

--- src/main/java/frc/robot/subsystems/drive/DriveWeights/MLVisionAngularAndHorizDriveWeight.java
+++ src/main/java/frc/robot/subsystems/drive/DriveWeights/MLVisionAngularAndHorizDriveWeight.java
@@ -18,12 +18,15 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class MLVisionAngularAndHorizDriveWeight implements DriveWeight {
 
-    private PIDController angularController = new PIDController(0.004, 0, 0); // Constants.PIDConstants.rotPID;
+    private PIDController angularController = new PIDController(0.005, 0, 0); // Constants.PIDConstants.rotPID;
     private PIDController horizontalController = new PIDController(0.004, 0, 0); // Constants.PIDConstants.rotPID;
 
-    private double angularVelocity;
-    private double horizontalVelocity;
-    private double verticalVelocity = 0.2; // ADD CONSTANT
+    private double angularVelocity = 0;
+    private double horizontalVelocity = 0;
+    
+    private double verticalVelocity = 0; // ADD CONSTANT
+    private double verticalVelocityConstant = 0.2; // ADD CONSTANT
+
     private MLVision vision;
     private Supplier<Rotation2d> angleSupplier;
 
@@ -31,7 +34,7 @@ public class MLVisionAngularAndHorizDriveWeight implements DriveWeight {
     // private Supplier<Rotation2d> correctedAngleSupplier;
 
     private double deadband = 0; // 0.1;
-    private double distanceLim = 10;
+    private double distanceLim = 5;
     private ChassisSpeeds chassisSpeedsToTurn = new ChassisSpeeds(0, 0, 0);
 
     private double initTime = 0;
@@ -47,43 +50,65 @@ public class MLVisionAngularAndHorizDriveWeight implements DriveWeight {
 
     @Override
     public ChassisSpeeds getSpeeds() {
+        horizontalVelocity = 0;
+        verticalVelocity = 0;
+        angularVelocity = 0;
 
-        angularVelocity = angularController.calculate(vision.getTX());
-        angularVelocity = (Math.abs(angularVelocity) < deadband) ? 0 : angularVelocity;
-        angularVelocity = MathUtil.clamp(angularVelocity, -1, 1);
-
-        horizontalVelocity = horizontalController.calculate(vision.getTX());
-        horizontalVelocity = (Math.abs(horizontalVelocity) < deadband) ? 0 : horizontalVelocity;
-        horizontalVelocity = MathUtil.clamp(horizontalVelocity, -1, 1);
-
-        // return chassisSpeedsToTurn;
         if (!vision.isTarget()) {
             //System.out.println(" NO Target " + vision.isTarget() + targetNoteSet);
-            chassisSpeedsToTurn = new ChassisSpeeds(0, 0, 0);
-            return chassisSpeedsToTurn;
-        }
+            horizontalVelocity = 0;
+            verticalVelocity = 0;
+            angularVelocity = 0;
+            chassisSpeedsToTurn = 
+            new ChassisSpeeds(0 , 0, 0); 
 
-        if (Math.abs(vision.getTX()) > distanceLim) {
-            chassisSpeedsToTurn = new ChassisSpeeds(0, 0, angularVelocity);
+        }
+        else if (Math.abs(vision.getTX()) > distanceLim) {
+            System.out.println(" TX IS " + vision.getTX() + "GREATER THAN dist linm " + distanceLim);
+            System.out.println(" help");
+            
+            horizontalVelocity = 0;
+            
+            verticalVelocity = 0;
+           
+            angularVelocity = angularController.calculate(vision.getTX());
+            angularVelocity = (Math.abs(angularVelocity) < deadband) ? 0 : angularVelocity;
+            angularVelocity = MathUtil.clamp(angularVelocity, -1, 1);
+
+            chassisSpeedsToTurn = 
+            new ChassisSpeeds(0 , 0, angularVelocity); 
 
             //deltaTX = vision.getTX() - previousTX;
             //Logger.recordOutput("MLVision/Delta TX", deltaTX);
-            Logger.recordOutput("MLVision/Input Rotational Velocity", angularVelocity);
+            
         }
+        else {
 
-    /* } else if (!isDriveToNoteFinished()) {
+            horizontalVelocity = horizontalController.calculate(vision.getTX());
+            horizontalVelocity = (Math.abs(horizontalVelocity) < deadband) ? 0 : horizontalVelocity;
+            horizontalVelocity = MathUtil.clamp(horizontalVelocity, -1, 1);
+            
+            //verticalVelocity = verticalVelocityConstant;
+            verticalVelocity = 0.2;
+
+            angularVelocity = 0;
+            
             chassisSpeedsToTurn = ChassisSpeeds.fromRobotRelativeSpeeds(
             new ChassisSpeeds(-verticalVelocity, -horizontalVelocity, 0),
-             angleSupplier.get());
-                return chassisSpeedsToTurn; 
-    }
-    */ 
+             angleSupplier.get()); 
+        }
+     
+        Logger.recordOutput("MLVision/Input Rotational Velocity", angularVelocity);
+        Logger.recordOutput("MLVision/Input Horizontal Velocity", horizontalVelocity);
+        Logger.recordOutput("MLVision/Input Vertical Velocity", verticalVelocity);
+
+               
         
         return chassisSpeedsToTurn;
     }
     
 
-    public boolean isDriveToNoteFinished() { // add intake limit later
+    private boolean isDriveToNoteFinished() { // add intake limit later
 
         if (vision.isTarget()) {
             return false;
