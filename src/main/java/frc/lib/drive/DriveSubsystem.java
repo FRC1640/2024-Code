64d5d380b294db8f4d2fff83e254f6e0d3c67fc2
jdugan0 <nitsujwriter@gmail.com>
2024-03-01 18:57:18 -0500

    working single amp auto

--- src/main/java/frc/lib/drive/DriveSubsystem.java
+++ src/main/java/frc/lib/drive/DriveSubsystem.java
@@ -162,7 +162,7 @@ public class DriveSubsystem extends SubsystemBase {
         for (AprilTagVision vision : visions) {
             if (vision.isTarget() && vision.isPoseValid(vision.getAprilTagPose2d())) {
                 Logger.recordOutput("Distance to april tag", vision.getDistance());
-                double distConst = Math.pow(vision.getDistance(), 2.0); // distance standard deviation constant
+                double distConst = Math.pow(vision.getDistance() * 1.5, 2.0); // distance standard deviation constant
 
                 double velConst = Math.pow(Math.hypot(SwerveDriveDimensions.kinematics.toChassisSpeeds(
                         getActualSwerveStates()).vxMetersPerSecond,
