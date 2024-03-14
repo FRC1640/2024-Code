333248a0601230545cdacb61fb70da2ee08a99ec
renieharris <renieharris124@gmail.com>
2024-03-13 20:45:22 -0400

    ml vis constraints command

--- src/main/java/frc/robot/sensors/Vision/MLVision/MLVisionAutonCommand.java
+++ src/main/java/frc/robot/sensors/Vision/MLVision/MLVisionAutonCommand.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.lib.drive.DriveSubsystem;
 import frc.robot.Constants.PIDConstants;
 
