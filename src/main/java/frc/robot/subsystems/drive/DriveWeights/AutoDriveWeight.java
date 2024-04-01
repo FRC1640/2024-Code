74856672bc897adfd1203da0b272e12302c5a013
tgschmale <tylergschmale@gmail.com>
2024-04-01 16:57:11 -0400

    minor changes

--- src/main/java/frc/robot/subsystems/drive/DriveWeights/AutoDriveWeight.java
+++ src/main/java/frc/robot/subsystems/drive/DriveWeights/AutoDriveWeight.java
@@ -53,7 +53,7 @@ public class AutoDriveWeight implements DriveWeight {
         double xSpeed = (Math.cos(angle) * s / scale);
         double ySpeed = (Math.sin(angle) * s / scale);
         double offset = gyro.getOffset();
-        ChassisSpeeds cspeeds = new ChassisSpeeds(xSpeed*Math.cos(offset)+ySpeed*Math.sin(offset), ySpeed*Math.cos(offset)+xSpeed*Math.sin(offset), o);
+        ChassisSpeeds cspeeds = new ChassisSpeeds(xSpeed*Math.cos(offset)+ySpeed*Math.sin(offset), ySpeed*Math.cos(offset)-xSpeed*Math.sin(offset), o);
         return cspeeds;
     }
     @Override
