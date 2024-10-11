366865a2443b261073d7463300895635dad8b876
jdugan0 <justin.d.dugan@gmail.com>
2024-10-11 19:16:55 -0400

    improved double cone clarity

--- src/main/java/frc/lib/drive/SwerveAlgorithms.java
+++ src/main/java/frc/lib/drive/SwerveAlgorithms.java
@@ -13,101 +13,108 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.Constants.SwerveDriveDimensions;
 
 public class SwerveAlgorithms {
-    public static double maxNorm = computeMaxNorm(SwerveDriveDimensions.positions, new Translation2d(0,0));
-    public static SwerveModuleState[] desaturated(double xSpeed, double ySpeed, double rot, double currentAngleRadians, boolean fieldRelative){
+    public static double maxNorm = computeMaxNorm(SwerveDriveDimensions.positions, new Translation2d(0, 0));
+
+    public static SwerveModuleState[] desaturated(double xSpeed, double ySpeed, double rot, double currentAngleRadians,
+            boolean fieldRelative) {
         var swerveModuleStates = SwerveDriveDimensions.kinematics.toSwerveModuleStates(
-            fieldRelative
-                ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot,
-                new Rotation2d(currentAngleRadians))
-                : new ChassisSpeeds(xSpeed, ySpeed, rot));
+                fieldRelative
+                        ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot,
+                                new Rotation2d(currentAngleRadians))
+                        : new ChassisSpeeds(xSpeed, ySpeed, rot));
         SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, SwerveDriveDimensions.maxSpeed);
         return swerveModuleStates;
     }
 
-    public static SwerveModuleState[] desaturated(double xSpeed, double ySpeed, double rot, 
-    double currentAngleRadians, boolean fieldRelative, Translation2d centerOfRotation){
+    public static SwerveModuleState[] rawSpeeds(double xSpeed, double ySpeed, double rot) {
+        var swerveModuleStates = SwerveDriveDimensions.kinematics
+                .toSwerveModuleStates(new ChassisSpeeds(xSpeed, ySpeed, rot));
+        return swerveModuleStates;
+    }
+
+    public static SwerveModuleState[] desaturated(double xSpeed, double ySpeed, double rot,
+            double currentAngleRadians, boolean fieldRelative, Translation2d centerOfRotation) {
 
         var swerveModuleStates = SwerveDriveDimensions.kinematics.toSwerveModuleStates(
-            fieldRelative
-                ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot,
-                new Rotation2d(currentAngleRadians))
-                : new ChassisSpeeds(xSpeed, ySpeed, rot), centerOfRotation);
+                fieldRelative
+                        ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot,
+                                new Rotation2d(currentAngleRadians))
+                        : new ChassisSpeeds(xSpeed, ySpeed, rot),
+                centerOfRotation);
         SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, SwerveDriveDimensions.maxSpeed);
         return swerveModuleStates;
     }
-    public static SwerveModuleState[] rawSpeeds(double xSpeed, double ySpeed, double rot){
-        var swerveModuleStates = SwerveDriveDimensions.kinematics.toSwerveModuleStates(new ChassisSpeeds(xSpeed, ySpeed, rot));
-        return swerveModuleStates;
-    }
-    public static SwerveModuleState[] doubleCone(double xSpeed, double ySpeed, double rot, 
-    double currentAngleRadians, boolean fieldRelative){
+
+    public static SwerveModuleState[] doubleCone(double xSpeed, double ySpeed, double rot,
+            double currentAngleRadians, boolean fieldRelative) {
 
         double translationalSpeed = Math.hypot(xSpeed, ySpeed);
         double linearRotSpeed = Math.abs(rot * maxNorm);
         double k;
-        if (linearRotSpeed == 0 || translationalSpeed == 0){
+        if (linearRotSpeed == 0 || translationalSpeed == 0) {
             k = 0;
-        }
-        else{
-            k = Math.min(translationalSpeed / linearRotSpeed, linearRotSpeed / translationalSpeed);
+        } else {
+            k = Math.max(linearRotSpeed, translationalSpeed) / (linearRotSpeed + translationalSpeed);
         }
         double scale = 1 / (1 + k);
         var swerveModuleStates = SwerveDriveDimensions.kinematics.toSwerveModuleStates(
-            fieldRelative 
-                ? ChassisSpeeds.fromFieldRelativeSpeeds(scale * xSpeed, scale * ySpeed, scale * rot, 
-                new Rotation2d(currentAngleRadians)) 
-                : new ChassisSpeeds(xSpeed * scale, ySpeed * scale, rot * scale));
+                fieldRelative
+                        ? ChassisSpeeds.fromFieldRelativeSpeeds(scale * xSpeed, scale * ySpeed, scale * rot,
+                                new Rotation2d(currentAngleRadians))
+                        : new ChassisSpeeds(xSpeed * scale, ySpeed * scale, rot * scale));
         return swerveModuleStates;
     }
 
-    public static SwerveModuleState[] doubleCone(double xSpeed, double ySpeed, double rot, 
-    double currentAngleRadians, boolean fieldRelative, Translation2d centerOfRotation, boolean lockRotation){
-        
+    public static SwerveModuleState[] doubleCone(double xSpeed, double ySpeed, double rot,
+            double currentAngleRadians, boolean fieldRelative, Translation2d centerOfRotation, boolean lockRotation) {
+
         double translationalSpeed = Math.hypot(xSpeed, ySpeed);
-        
+
         double linearRotSpeed = Math.abs(rot * computeMaxNorm(SwerveDriveDimensions.positions, centerOfRotation));
         double k;
-        if (linearRotSpeed == 0 || translationalSpeed == 0){
+
+        // determine scaling factor for double cone map
+        if (linearRotSpeed == 0 || translationalSpeed == 0) {
             k = 0;
-        }
-        else{
+        } else {
             k = Math.min(translationalSpeed / linearRotSpeed, linearRotSpeed / translationalSpeed);
         }
-        if (lockRotation){
+        if (lockRotation) {
             double scale = Math.abs((1 - Math.abs(linearRotSpeed) / (SwerveDriveDimensions.maxSpeed)));
             Logger.recordOutput("ScaleDriveWeight", scale);
             var swerveModuleStates = SwerveDriveDimensions.kinematics.toSwerveModuleStates(
-                fieldRelative 
-                    ? ChassisSpeeds.fromFieldRelativeSpeeds(scale * xSpeed, scale * ySpeed, rot, 
-                    new Rotation2d(currentAngleRadians)) 
-                    : new ChassisSpeeds(xSpeed * scale, ySpeed * scale, rot), centerOfRotation);
+                    fieldRelative
+                            ? ChassisSpeeds.fromFieldRelativeSpeeds(scale * xSpeed, scale * ySpeed, rot,
+                                    new Rotation2d(currentAngleRadians))
+                            : new ChassisSpeeds(xSpeed * scale, ySpeed * scale, rot),
+                    centerOfRotation);
             return swerveModuleStates;
-        }
-        else{
+        } else {
             double scale = 1 / (1 + k);
             var swerveModuleStates = SwerveDriveDimensions.kinematics.toSwerveModuleStates(
-                fieldRelative 
-                    ? ChassisSpeeds.fromFieldRelativeSpeeds(scale * xSpeed, scale * ySpeed, scale * rot, 
-                    new Rotation2d(currentAngleRadians)) 
-                    : new ChassisSpeeds(xSpeed * scale, ySpeed * scale, rot * scale), centerOfRotation);
+                    fieldRelative
+                            ? ChassisSpeeds.fromFieldRelativeSpeeds(scale * xSpeed, scale * ySpeed, scale * rot,
+                                    new Rotation2d(currentAngleRadians))
+                            : new ChassisSpeeds(xSpeed * scale, ySpeed * scale, rot * scale),
+                    centerOfRotation);
             return swerveModuleStates;
         }
-        
+
     }
-    
+
     public static double computeMaxNorm(Translation2d[] translations, Translation2d centerOfRotation) {
         return Arrays.stream(translations)
-            .map((translation) -> translation.minus(centerOfRotation))
-            .mapToDouble(Translation2d::getNorm)
-            .max()
-            .orElseThrow(() -> new NoSuchElementException("No max norm."));
+                .map((translation) -> translation.minus(centerOfRotation))
+                .mapToDouble(Translation2d::getNorm)
+                .max()
+                .orElseThrow(() -> new NoSuchElementException("No max norm."));
     }
 
     public static double angleDistance(double angle1, double angle2) {
         double rawDifference = angle2 - angle1;
         // Normalize the difference within the range [-pi, pi]
         double normalizedDifference = ((rawDifference + Math.PI) % (2 * Math.PI)) - Math.PI;
-        
+
         // Adjust for cases where the difference is exactly -pi
         if (normalizedDifference < -Math.PI) {
             normalizedDifference += 2 * Math.PI;
