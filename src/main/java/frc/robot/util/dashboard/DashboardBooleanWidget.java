21d0c4a947b5dea2302e02e51a3728d99f223d5a
1023941311414 <jake.widmann@outlook.com>
2024-11-30 16:23:29 -0500

    A few more changes

--- src/main/java/frc/robot/util/dashboard/DashboardBooleanWidget.java
+++ src/main/java/frc/robot/util/dashboard/DashboardBooleanWidget.java
@@ -1,20 +1,25 @@
 package frc.robot.util.dashboard;
 
+import java.util.ArrayList;
+import java.util.Vector;
 import java.util.function.BooleanSupplier;
-
 import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.IntegerArrayEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTablesJNI;
+import edu.wpi.first.networktables.Publisher;
 
 public class DashboardBooleanWidget implements DashboardWidget {
     private NetworkTable metadataSubtable;
-    // private NetworkTable dataSubtable;
+    // private ArrayList<Publisher> metadataPublishers;
+    // private Vector<Integer> size = new Vector<>();
     private BooleanPublisher publisher;
     private BooleanSupplier valueSupplier;
 
-    // I'm ignoring configuration options for now and setting the name of the widget through name in the constructor.
+    // Ignoring configuration options for now and setting the name of the widget through name in the constructor.
     public DashboardBooleanWidget(String name, NetworkTable metadata, NetworkTable data, BooleanSupplier valueSupplier) {
         metadataSubtable = metadata.getSubTable(name);
+        // metadataPublishers = new ArrayList<>();
         publisher = data.getBooleanTopic(name).publish();
         this.valueSupplier = valueSupplier;
     }
@@ -22,5 +27,15 @@ public class DashboardBooleanWidget implements DashboardWidget {
     @Override
     public void periodic() {
         publisher.set(valueSupplier.getAsBoolean(), NetworkTablesJNI.now());
+        // for (Publisher publisher : metadataPublishers) {
+        //     publisher.set(, NetworkTablesJNI.now());
+        // }
     }
+
+    // @Override
+    // public DashboardBooleanWidget withSize(int x, int y) {
+    //     Publisher publisher = metadataSubtable.getIntegerArrayTopic("size").publish();
+    //     metadataPublishers.add(publisher);
+    //     return this;
+    // }
 }
--- src/main/java/frc/robot/util/dashboard/DashboardWidget.java
+++ src/main/java/frc/robot/util/dashboard/DashboardWidget.java
@@ -2,4 +2,6 @@ package frc.robot.util.dashboard;
 
 public interface DashboardWidget {
     public default void periodic() { }
+
+    public default DashboardWidget withSize(int x, int y) { return null; }
 }
